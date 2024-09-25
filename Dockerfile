34ec851bf84a4e081eb5bb4b2ced279fc6f5fa6c
ivanzharinov <ivanzharinoff@yandex.ru>
2024-09-25 20:03:21 +0300

    step 2

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,18 @@
-FROM golang:1.20 AS builder
+FROM golang:1.23 AS builder
 
-# Устанавливаем рабочую директорию
 WORKDIR /app
 
-# Копируем файлы go.mod и go.sum и устанавливаем зависимости
-COPY go.mod go.sum ./
+COPY go.mod .
+COPY go.sum .
 RUN go mod download
 
-# Копируем остальной исходный код
 COPY . .
 
-# Собираем двоичный файл
-RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main .
+RUN CGO_ENABLED=0 GOOS=linux go build -o /app/main .
 
-# Используем минимальный образ для финального контейнера
 FROM scratch
 
-# Копируем собранный бинарник из предыдущего стадии
 COPY --from=builder /app/main /main
 COPY --from=builder /app/web /web
 
-# Указываем команду запуска
-CMD ["/main"]
+ENTRYPOINT ["/main"]
--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/go-chi/chi/v5"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
+	_ "modernc.org/sqlite"
 )
 
 func main() {
+	CheckDB()
 	port := os.Getenv("TODO_PORT")
 	webDir := "./web"
 
@@ -18,4 +24,53 @@ func main() {
 
 	fmt.Printf("Сервер запущен и слушает порт %s", port)
 	http.ListenAndServe(":"+port, r)
+}
+
+func CheckDB() {
+	appPath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbFile := filepath.Join(filepath.Dir(appPath), "scheduler.db")
+	_, err = os.Stat(dbFile)
+
+	var install bool
+	if err != nil {
+		install = true
+	}
+	if install {
+		create()
+	} else {
+		fmt.Println("Таблица уже существует")
+	}
+}
+
+func create() {
+	db, err := sql.Open("sqlite", "./scheduler.db")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer db.Close()
+
+	createTableSQL := `
+    CREATE TABLE IF NOT EXISTS scheduler (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date CHAR(8) NOT NULL,
+        title TEXT NOT NULL,
+        comment TEXT,
+        repeat VARCHAR(128),
+        UNIQUE(date, title)
+    );
+
+    CREATE INDEX IF NOT EXISTS idx_date ON scheduler (date);
+    `
+
+	_, err = db.Exec(createTableSQL)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println("Таблица создана успешно")
 }
